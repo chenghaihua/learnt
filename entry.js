fba209b88d3d238d31f0cc63626b2b2d25eddf08
chenghaihua <834037683@qq.com>
2017-07-25 17:17:10 +0800

    第一次提交

---
+++ entry.js
@@ -0,0 +1,7 @@
+/**
+ * Created by chenghaihua on 2017/3/6.
+ */
+var name=require('./name');
+
+require('style!css!./style.css');alert()
+document.getElementById('app').textContent='hello~'+name;
---
+++ name.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chenghaihua on 2017/3/6.
+ */
+module.exports="nihao.net";
